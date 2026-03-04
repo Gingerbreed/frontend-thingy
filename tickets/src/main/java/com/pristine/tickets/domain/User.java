091e2f3b4b7cd619371ab6b367a6e5b09e8c795a
Morgan <morgan@mbaker.org>
2026-03-04 01:57:51 -0500

    add events and update user class

--- tickets/src/main/java/com/pristine/tickets/domain/User.java
+++ tickets/src/main/java/com/pristine/tickets/domain/User.java
@@ -1,14 +1,13 @@
 package com.pristine.tickets.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -29,11 +28,24 @@ public class User {
     @Column(name = "email", nullable = false, unique = true)
     private String email;
 
-    //TODO: Organized Events
+    @OneToMany(mappedBy = "organizer", cascade = CascadeType.ALL)
+    private List<Event> organizedEvents = new ArrayList<>();
 
-    //TODO: Attended Events
+    @ManyToMany
+    @JoinTable(
+          name =  "user_attending_events",
+          joinColumns = @JoinColumn(name = "user_id"),
+          inverseJoinColumns = @JoinColumn(name = "event_id")
+    )
+    private List<Event> attendingEvents = new ArrayList<>();
 
-    //TODO: Staffing Events
+  @ManyToMany
+  @JoinTable(
+    name =  "user_staffing_events",
+    joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name = "event_id")
+  )
+    private List<Event> staffingEvents = new ArrayList<>();
 
     @CreatedDate
     @Column(name = "created_at", updatable = false, nullable = false)
